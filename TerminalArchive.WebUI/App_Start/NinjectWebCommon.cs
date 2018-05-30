3d72326778f02a4a31f4f4c786229d12027db10d
mi1vus <milvus@e1.ru>
2018-05-30 19:00:17 +0500

    infrustruct

--- TerminalArchive.WebUI/App_Start/NinjectWebCommon.cs
+++ TerminalArchive.WebUI/App_Start/NinjectWebCommon.cs
@@ -53,6 +53,8 @@ namespace TerminalArchive.WebUI.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-        }        
-    }
+            System.Web.Mvc.DependencyResolver.SetResolver(new
+                TerminalArchive.WebUI.Infrastructure.NinjectDependencyResolver(kernel));
+        }
+    }        
 }
